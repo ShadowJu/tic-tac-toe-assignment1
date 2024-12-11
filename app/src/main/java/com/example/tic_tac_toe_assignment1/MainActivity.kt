2ba8155a7473cef1077b9e9bd9f6355ca462256f
ShadowJu <alikkhol@gmail.com>
2024-12-11 19:12:11 +0200

    feat: add OnButtonClick and resetGame fun

--- app/src/main/java/com/example/tic_tac_toe_assignment1/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe_assignment1/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.tic_tac_toe_assignment1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.GridLayout
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,9 +12,44 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.tic_tac_toe_assignment1.ui.theme.Tictactoeassignment1Theme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var gridLayout: GridLayout
+    private lateinit var statusText: TextView
+    private lateinit var playAgainButton: Button
+    private var buttons = arrayOfNulls<Button>(9)
+    private var xTurn = true
+    private var gameOver = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
+
+        gridLayout = findViewById(R.id.gridLayout)
+        statusText = findViewById(R.id.statusText)
+        playAgainButton = findViewById(R.id.playAgainButton)
+
+        for (i in 0 until 9) {
+            buttons[i] = gridLayout.getChildAt(i) as Button
+            buttons[i]?.setOnClickListener { onButtonClick(it as Button, i) }
+        }
+
+        // Play Again Button
+        playAgainButton.setOnClickListener { resetGame() }
+
+    }
+
+    private fun onButtonClick(button: Button, index: Int) {
+        if (button.text.isNotEmpty() || gameOver) return
+
+        button.text = if (xTurn) "X" else "O"
+        statusText.text = if (xTurn) "Player O's turn" else "Player X's turn"
+        xTurn = !xTurn
+
+    }
+    private fun resetGame() {
+        buttons.forEach { it?.text = "" }
+        statusText.text = "Player X's turn"
+        xTurn = true
+        gameOver = false
     }
 }
 
