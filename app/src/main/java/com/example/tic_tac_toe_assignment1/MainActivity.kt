8ea8f0163b8a00df0e0b3eda0f39c249bf4b0ea3
ShadowJu <alikkhol@gmail.com>
2024-12-15 14:58:20 +0200

    feat: fix some comments

--- app/src/main/java/com/example/tic_tac_toe_assignment1/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe_assignment1/MainActivity.kt
@@ -9,62 +9,45 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.tic_tac_toe_assignment1.ui.theme.Tictactoeassignment1Theme
 
 class MainActivity : ComponentActivity() {
     private lateinit var gridLayout: GridLayout
     private lateinit var statusText: TextView
-    private lateinit var playAgainButton: Button
+    private lateinit var restartButton: Button
     private var buttons = arrayOfNulls<Button>(9)
-    private var xTurn = true
-    private var gameOver = false
+    private var isXTurn = true
+    private var isGameOver = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
 
         gridLayout = findViewById(R.id.gridLayout)
         statusText = findViewById(R.id.statusText)
-        playAgainButton = findViewById(R.id.playAgainButton)
+        restartButton = findViewById(R.id.restartButton)
 
-        for (i in 0 until 9) {
+        for (i in 0 until gridLayout.childCount) {
             buttons[i] = gridLayout.getChildAt(i) as Button
             buttons[i]?.setOnClickListener { onButtonClick(it as Button, i) }
         }
 
         // Play Again Button
-        playAgainButton.setOnClickListener { resetGame() }
+        restartButton.setOnClickListener { resetGame() }
 
     }
 
     private fun onButtonClick(button: Button, index: Int) {
-        if (button.text.isNotEmpty() || gameOver) return
+        if (button.text.isNotEmpty()) return
 
-        button.text = if (xTurn) "X" else "O"
-        statusText.text = if (xTurn) "Player O's turn" else "Player X's turn"
-        xTurn = !xTurn
+        button.text = if (isXTurn) "X" else "O"
+        statusText.text = if (isXTurn) "Player O's turn" else "Player X's turn"
+        isXTurn = !isXTurn
 
     }
     private fun resetGame() {
         buttons.forEach { it?.text = "" }
         statusText.text = "Player X's turn"
-        xTurn = true
-        gameOver = false
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Tictactoeassignment1Theme {
-        Greeting("Android")
+        isXTurn = true
+        isGameOver = false
     }
 }
